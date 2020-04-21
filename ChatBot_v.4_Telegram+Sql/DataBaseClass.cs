b2a41d96f7dc3ed830659772e16641c210e6102b
Pavel <zeroblaze@yandex.ru>
2020-04-21 18:05:14 +0300

    Добавьте файлы проекта.

---
+++ ChatBot_v.4_Telegram+Sql/DataBaseClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace ChatBot_v._4_Telegram_Sql
+{
+    static class DataBaseClass
+    {
+        static SqlConnection connection;
+        static DataBaseClass()
+        {
+            string connectionString = 
+                @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=databaseTelegramBotLog;Integrated Security=True;Pooling=True";
+            connection = new SqlConnection(connectionString);
+            connection.Open();
+        }
+
+        public static void Add(Msg userMessage)
+        {
+            var sql = string.Format(
+                @"INSERT INTO MessageDB(userId, userFirstName, userText)
+                  VALUES ({0}, N'{1}', N'{2}')",
+                userMessage.id,
+                userMessage.name,
+                userMessage.text
+                );
+            SqlCommand sqlCommand = new SqlCommand(sql, connection);
+            sqlCommand.ExecuteNonQuery();
+            //var result = sqlCommand.ExecuteReader();
+        }
+        public static void Free() 
+        {
+            connection.Close();
+            connection.Dispose(); 
+        }
+    }
+}
---
+++ ChatBot_v.4_Telegram+Sql/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Telegram.Bot;
+using System.IO;
+using System.Windows.Automation;
+using System.Net;
+using System.Net.Http;
+
+namespace ChatBot_v._4_Telegram_Sql
+{
+    class Msg
+    {
+        public long id;
+        public string name;
+        public string text;
+
+        public override string ToString()
+        {
+            return $"{name}: {text}";
+        }
+    }
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        TelegramBotClient bot;
+        WebProxy proxy;
+        HttpClientHandler httpClientHandler;
+        HttpClient hc;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            proxy = new WebProxy(Address: new Uri("http://163.172.189.32:8811"));
+            httpClientHandler = new HttpClientHandler()
+            {
+                Proxy = proxy
+            };
+            hc = new HttpClient(handler: httpClientHandler, disposeHandler: true);
+
+            bot = new TelegramBotClient(File.ReadAllText("token.txt"));
+            bot.OnMessage += Bot_OnMessage;
+            bot.StartReceiving();
+        }
+
+        private void Bot_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+            Msg currentMsg = new Msg()
+            {
+                id = e.Message.Chat.Id,
+                name = e.Message.Chat.FirstName,
+                text = e.Message.Text
+            };
+            DataBaseClass.Add(currentMsg);
+
+            this.Dispatcher.Invoke(()=> listMsg.Items.Add(currentMsg));
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            long id = ((Msg)listMsg.SelectedItem).id;
+
+            bot.SendTextMessageAsync(id, tbSendMessage.Text);
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            DataBaseClass.Free();
+        }
+    }
+}
